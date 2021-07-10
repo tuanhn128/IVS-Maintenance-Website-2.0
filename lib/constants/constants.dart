3affd4453a910f4504c6fb6e9f852fc7eccd019e
tuanhn128 <tuanhn@stanford.edu>
2021-07-09 22:00:18 -0700

    feat: added printer cord as an option for item name in problems

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,9 +1,9 @@
-/* TODO: Verify this list of item names */
 const List<String> kItemNames = [
   'Printer',
   'Printer Cartridge',
   'Headphones',
   'Laptop',
   'Laptop Charger',
-  'Keypad'
+  'Keypad',
+  'Printer Cord'
 ];
